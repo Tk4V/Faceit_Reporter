2c281af8e30a8ad19ec3ee445f7a1d9340ecdfeb
233 <2323>
2024-02-10 15:12:26 +0200

    text recognition was added

---
+++ Text_recognition_from_image.py
@@ -0,0 +1,29 @@
+from PIL import Image
+import pytesseract
+
+def perform_ocr(image_path):
+    # Open the image file
+    img = Image.open(image_path)
+
+    # Perform OCR on the image
+    text = pytesseract.image_to_string(img)
+
+    return text
+
+def save_to_file(text, output_file="output.txt"):
+    # Save the text to a file
+    with open(output_file, 'w', encoding='utf-8') as file:
+        file.write(text)
+
+if __name__ == "__main__":
+    # Replace 'your_image_path.jpg' with the actual path to your image file
+    image_path = '730_20240210133856_1.png'
+
+    # Perform OCR on the image
+    result_text = perform_ocr(image_path)
+
+    # Save the recognized text to a file
+    save_to_file(result_text, output_file="output.txt")
+
+    # Print a confirmation message
+    print(f"Recognized text saved to 'output.txt'")
